66d9a8c5662866a10b8d565bed346936d31ec97a
NandaDwiCahyo <nandad.cahyo@gmail.com>
2020-09-08 14:43:23 +0700

    Routing
    Belajar routing menggunakan laravel

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
+
+// CLOSURE
+Route::get('/profil', function () {
+	return "Profil Siswa";
+})->name('profil');
+
+// ROUTE NAME
+Route::get('/testname', function () {
+	return route('profil');
+});
+
+// PARAMETER
+Route::get('/profil/{id}', function ($id) {
+	return $id;
+});
+
+// CONTROLLER
+Route::get('/controller', 'PagesController@show');
+
+// RESOURCE
+Route::resource('students', 'StudentController');
